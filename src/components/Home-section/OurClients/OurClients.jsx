33632e6948ea41270d29db9be87ca6e8dab43db3
Pranish Shrestha <pranishshrestha181@gmail.com>
2022-11-07 14:39:46 +0545

    feat: added prev,next button on clients and responsive

--- src/components/Home-section/OurClients/OurClients.jsx
+++ src/components/Home-section/OurClients/OurClients.jsx
@@ -4,6 +4,14 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { logo } from "./our-clients";
 import "./ourClients.css";
+const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
+  <img src="clients-arrow.png" alt="prevArrow" {...props} />
+);
+
+const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
+  <img src="clients-arrow-right.png" alt="nextArrow" {...props} />
+);
+
 function OurClients() {
   const settings = {
     dots: false,
@@ -15,7 +23,9 @@ function OurClients() {
     autoplay: true,
     speed: 3000,
     autoplaySpeed: 8000,
-    cssEase: "linear",
+    // cssEase: "linear",
+    nextArrow: <SlickArrowRight />,
+    prevArrow: <SlickArrowLeft />,
     responsive: [
       {
         breakpoint: 768,
@@ -34,15 +44,17 @@ function OurClients() {
   };
 
   return (
-    
     <div className="ourClients_container py-4">
       <h2 className="text-center py-1">Our Clients</h2>
       <Slider {...settings} className="d-flex flex-column">
         {" "}
         {logo.map((item) => (
-          <div className="company-logo d-flex flex-column justify-content-center align-items-center" key={item.id}>
+          <div
+            className="company-logo d-flex flex-column justify-content-center align-items-center"
+            key={item.id}
+          >
             <div className="clients_logo">
-            <img src={item.image} alt="company-logo" />
+              <img src={item.image} alt="company-logo" />
             </div>
             <p>{item.name}</p>
           </div>
